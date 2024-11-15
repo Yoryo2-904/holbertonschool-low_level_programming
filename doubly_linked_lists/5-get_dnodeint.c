72ff1411888a6ada25ad62a1509a8e1970fb7fd7
yoryo <lealjorge2904@gmail.com>
2024-11-14 19:58:38 -0500

    5-get_dnodeint.c fixes 2

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -12,13 +12,13 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int i = 0;
 	dlistint_t *temp = head;
 
+	if (i > index)
+		return (NULL);
 	while (i < index && temp->next)
 	{
 		temp = temp->next;
 		i++;
 	}
-	if (temp == NULL && i > index)
-		return (NULL);
 
 	return (temp);
 }
