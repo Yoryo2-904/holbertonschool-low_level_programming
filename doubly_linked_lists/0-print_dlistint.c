1ebf81fa566059aa5a01d0abb0c69d4831456abc
yoryo <lealjorge2904@gmail.com>
2024-11-14 19:07:29 -0500

    0, 1, 2, 3,

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -12,8 +12,6 @@ size_t print_dlistint(const dlistint_t *h)
 	dlistint_t *temp = (dlistint_t *) h;
 
 	for (i = 0; temp; i++, temp = temp->next)
-	{
-			printf("%d\n", temp->n);
-	}
+		printf("%d\n", temp->n);
 	return (i);
 }
