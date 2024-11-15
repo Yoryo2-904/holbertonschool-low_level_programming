8a16a7ab0c6b5d51918876090e83ecc6ae157572
yoryo <lealjorge2904@gmail.com>
2024-11-14 19:50:23 -0500

    5-get_dnodeint.c

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - goes through the list until index's node
+ * @head: the reference to go through the list
+ * @index: the index to move on
+ *
+ * Return: the index's node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *temp = head;
+
+	while (i < index && temp->next)
+	{
+		temp = temp->next;
+		i++;
+	}
+	if (temp == NULL)
+		return (NULL);
+
+	return (temp);
+}
